23e29e1dec43d0ad449f9193387186999d1e2623
Alisa Proshchenko <ajanglaide@gmail.com>
2020-04-27 17:51:56 +0300

    comments were added

--- bfs.pl
+++ bfs.pl
@@ -1,89 +1,66 @@
-initial_state([0,0]).
+initial_state([0,0]). %start state -- empty buckets
 
-final_state([3,_]).
+final_state([3,_]). %goals
 final_state([_,3]).
 
-main():-
+main():- %program start
     initial_state(State),
     bfs(State-[],[],[]).
 
-bfs(State-[],History,[]):-
+bfs(State-[],History,[]):- %for start state - empty queue for further bfs
     append_cor(History,State,NewHistory),
     findall(X-[State], (search_states(State,X),not(member(X,NewHistory))),[NextState-P|T]),
     bfs(NextState-[P],NewHistory,T).
-bfs(State-Path,_,[_]):-
+bfs(State-Path,_,[_]):-   %goal is found in bfs
     final_state(State),
     append_cor(Path,State,FinalPath),
     write(FinalPath).
-bfs(State-Path,History,[Queue-PQ|Tail]):-
+bfs(State-Path,History,[Queue-PQ|Tail]):- %bfs (1 path length of the next state)
     length(PQ,0), %brackets output problem solver
     append_cor(History,State,NewHistory),
     findall(X-P1, (search_states(State,X),not(member(X,NewHistory)),append(Path,[State],P1)),[NextStates-P|T]),
     append(Tail,[NextStates-P|T],NewQueue),
     bfs(Queue-[PQ],NewHistory,NewQueue).
-bfs(State-Path,History,[Queue-PQ|Tail]):-
+bfs(State-Path,History,[Queue-PQ|Tail]):- %bfs (more than 1 path length of the next state)
     append_cor(History,State,NewHistory),
     findall(X-P1, (search_states(State,X),not(member(X,NewHistory)),append(Path,[State],P1)),[NextStates-P|T]),
     append(Tail,[NextStates-P|T],NewQueue),
     bfs(Queue-PQ,NewHistory,NewQueue).
-bfs(State-_,History,[Queue-PQ|Tail]):-
+bfs(State-_,History,[Queue-PQ|Tail]):- %there is no any next state
     append_cor(History,State,NewHistory),
     bfs(Queue-PQ,NewHistory,Tail).
 
 
-
-resolve_path([],_,_):-!.
-resolve_path([NextState|T],[State|Path], FinalStates):-
-    append_cor([NextState],State,Upd1),
-    append(Upd1,Path,Upd2),
-    resolve_path(T,[State|Path],_),
-    append_cor(FinalStates,Upd2,FinalStates).
-
-
-
-append_cor(Queue,[L5,L9],Q):-
+append_cor(Queue,[L5,L9],Q):- %helper for correct append of states
     append(Queue,[[L5,L9]],Q).
 
-next_front([State-Move|NextMoves],NextFront,[s(State,[Move|Path])|NF1],History,Path):-
-    not(member(State,History)),
-    !,
-    next_front(NextMoves,NextFront,NF1,History,Path).
-next_front([_|NextMoves],NextFront,NF1,History,Path):-
-    next_front(NextMoves,NextFront,NF1,History,Path).
-next_front([],NextFront,NextFront,_,_).
-
-history(State,History,History):-
-    final_state(State),
-    !.
-history(State,History,[State|History]).
-
-search_states([L5,L9],[NL5,NL9]):-
+search_states([L5,L9],[NL5,NL9]):- %get next states
     state([L5,L9],[NL5,NL9]).
 
-state([L5,L9],[5,L9]):-
+state([L5,L9],[5,L9]):- %fill up L5
     L5 = 0.
-state([L5,L9],[L5,9]):-
+state([L5,L9],[L5,9]):- %fill up L9
     L9 = 0.
-state([L5,L9],[0,L9]):-
+state([L5,L9],[0,L9]):- %pour out L5
     not(L5 = 0).
-state([L5,L9],[L5,0]):-
+state([L5,L9],[L5,0]):- %pour out L9
     not(L9 = 0).
-state([L5,L9],[NL5,9]):-
+state([L5,L9],[NL5,9]):- %pour from L5 to L9, L9 is filled up
     not(L5 = 0),
     not(L9 = 9),
     L5 + L9 > 9,
     NL5 is L5 + L9 - 9.
-state([L5,L9],[0,NL9]):-
+state([L5,L9],[0,NL9]):- %pour from L5 to L9, L5 is empty
     not(L5 = 0),
     not(L9 = 9),
     L5 + L9 =< 9,
     NL9 is L5 + L9.
-state([L5,L9],[5,NL9]):-
+state([L5,L9],[5,NL9]):-  %pour from L9 to L5, L5 is filled up
     not(L5 = 5),
     not(L9 = 0),
     L5 + L9 > 5,
     NL9 is L5 + L9 - 5.
-state([L5,L9],[NL5,0]):-
+state([L5,L9],[NL5,0]):- %pour from L9 to L5, L9 is empty
     not(L5 = 5),
     not(L9 = 0),
     L5 + L9 =< 5,
